8b89439f3a3418990dc0732f32c62e5663ff8106
sdelaguera <99196793+sdelaguera@users.noreply.github.com>
2022-03-11 14:01:23 -0500

    added section three

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const checkpoint = 300;
+const learnedBtn = document.getElementById("i-learned")
+const skillList = document.getElementById("skill-list")
+const clickMe = document.getElementById("click-me")
  
 window.addEventListener("scroll", () => {
   const currentScroll = window.pageYOffset;
@@ -19,3 +22,23 @@ window.addEventListener("scroll", () => {
     }
     document.querySelector(".containerThree").style.opacity = opacity;
   });
+
+  window.addEventListener("scroll", () => {
+    const currentScroll = window.pageYOffset;
+    if (currentScroll <= checkpoint + 900) {
+      opacity = 0 - currentScroll / checkpoint;
+    } else {
+      opacity = 1;
+    }
+    clickMe.style.opacity = opacity;
+  });
+
+
+learnedBtn.addEventListener("click", function () {
+    if (skillList.style.visibility = "none") {
+        skillList.style.visibility = "visible";
+        skillList.scrollIntoView({behavior: "smooth", block: "end"});
+    } else {
+        skillList.style.visibility = "none";
+    }    
+});

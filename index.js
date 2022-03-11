9f7e4f441cc2d6a0c92328b499b66ee8758c1d01
sdelaguera <99196793+sdelaguera@users.noreply.github.com>
2022-03-11 12:45:56 -0500

    section two finished

--- index.js
+++ index.js
@@ -7,5 +7,15 @@ window.addEventListener("scroll", () => {
   } else {
     opacity = 1;
   }
-  document.querySelector(".containerTwo", ".containerThree").style.opacity = opacity;
+  document.querySelector(".containerTwo").style.opacity = opacity;
 });
+
+window.addEventListener("scroll", () => {
+    const currentScroll = window.pageYOffset;
+    if (currentScroll <= checkpoint + 600) {
+      opacity = 0 - currentScroll / checkpoint;
+    } else {
+      opacity = 1;
+    }
+    document.querySelector(".containerThree").style.opacity = opacity;
+  });
